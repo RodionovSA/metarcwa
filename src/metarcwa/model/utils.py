d86523742439acfd8c2099d97558246b04580d07
RodionovSA <sergeyrodionov93@gmail.com>
2026-06-09 14:22:20 +0300

    upd

--- src/metarcwa/model/utils.py
+++ src/metarcwa/model/utils.py
@@ -3,6 +3,7 @@
 
 import torch
 import torch.nn as nn
+from typing import Callable
 
 def register(module, name, value, dtype=torch.float32):
     """Register `value` on `module` under `name`.
@@ -15,3 +16,17 @@ def register(module, name, value, dtype=torch.float32):
         setattr(module, name, value)
     else:
         module.register_buffer(name, torch.as_tensor(value, dtype=dtype))
+        
+def from_metashapes(shape, **kwargs) -> Callable:
+    """ Takes Shape object from metshapes + rasterisation params (smoothness, ...) as kwargs 
+    and outputs shape_fn(lattice, nx, ny)
+    """
+    shape_fn = ...
+    return shape_fn
+
+def from_dispertorch(dispersion, **kwargs) -> Callable:
+    """ Takes ModelDispersion object from dispertorch + some kwargs 
+    and outputs eps_fn(wavelength). wavelength in **nm**
+    """
+    eps_fn = ...
+    return eps_fn
